846495239df3bf09ceb70436ed3f154c0a97f0f8
Shaswat Satyam <70892928+shaswat-satyam@users.noreply.github.com>
2025-01-17 09:52:26 +0530

    added url

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { useState } from "react";
 function App() {
   const [currentScreen, setCurrentScreen] = useState("about");
   return (
-    <div className="absolute top-0 z-[-2] min-h-screen  h-fit w-fit bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
+    <div className="absolute top-0 z-[-2] min-h-screen min-w  h-fit w-fit bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
       <div className="p-5 z-10 text-white flex flex-col gap-5">
         {currentScreen == "projects" ? <Subjects /> : <></>}
         <Header setScreen={setCurrentScreen} />
