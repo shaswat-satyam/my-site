0319f6e30baa002df6d572bf734acddcd0ba1672
Shaswat Satyam <shaswatsatyam51@gmail.com>
2025-01-30 22:54:46 +0530

    Started with Project tab

---
+++ src/projects/Projects.tsx
@@ -0,0 +1,12 @@
+export default function projects() {
+  return (
+    <div className="backdrop-blur-sm p-5 rounded-xl flex flex-col gap-5 border-gray-900 border-2">
+      <img
+        className="w-64"
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fblog.lootcrate.com%2Fwp-content%2Fuploads%2F2018%2F09%2FCells-at-Work-Platlets-3.gif&f=1&nofb=1&ipt=f1a7b3205a5a0d7ddd141ea5ee5ed0a5d6636c719a677214abd7292b817360e0&ipo=images"
+        alt="Work Under Progress"
+      />
+      <h1>Work in Progress</h1>
+    </div>
+  );
+}
