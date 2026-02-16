b29e074e6f1fa4ccdd76af5c42c59aec9b1ba2dd
Shaswat Satyam <70892928+shaswat-satyam@users.noreply.github.com>
2026-02-16 10:38:13 +0530

    Basic Index page

---
+++ src/consts.ts
@@ -0,0 +1,11 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Shaswat Satyam";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+export const NAME = "Shaswat Satyam";
+export const MAIL = "shaswatsatyam51@gmail.com";
+export const LOCATION = "Bengaluru, India";
+export const COMPANY = "IBM";
+export const ROLE = "Associate System Engineer";
+export const CURRENT_TECH = "Enterprise Level Java";
---
+++ src/socials.ts
@@ -0,0 +1,20 @@
+// General
+export const GITHUB = "https://www.github.com/shaswat-satyam";
+export const LINKEDIN = "https://www.linkedin.com/in/shaswat-satyam";
+export const BLUESKY = "https://www.bluesky.com/user/palmtrees2308";
+
+// Fediverse
+export const MASTODON = "https://www.mastodon.com/user/palmtrees2308";
+export const LEMMY = "https://www.lemmy.com/user/palmtrees2308";
+export const PEERTUBE = "https://www.peertube.com/user/palmtrees2308";
+export const BOOKWYRM = "https://www.bookwyrm.com/user/palmtrees2308";
+export const PIXELFED = "https://www.pixelfed.com/user/palmtrees2308";
+
+// Programming
+export const STACKOVERFLOW = "https://www.stackoverflow.com/user/palmtrees2308";
+export const LEETCODE = "https://www.leetcode.com/user/palmtrees2308";
+export const HACKERRANK = "https://www.hackerrank.com/user/palmtrees2308";
+export const KATAS = "https://www.codekata.com/user/palmtrees2308";
+export const DEVTO = "https://www.dev.to/user/palmtrees2308";
+export const REDDIT = "https://www.reddit.com/user/palmtrees2308";
+export const TWITTER = "https://www.x.com/user/palmtrees2308";
